83d3e76b602b96da6e0ba65774ad6e0060119a50
dolphin-13 <55006453+dolphin-13@users.noreply.github.com>
2021-12-03 23:57:57 -0800

    Updated after code review

--- test/com/coderscampus/assignment/Assignment8Test.java
+++ test/com/coderscampus/assignment/Assignment8Test.java
@@ -5,11 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
@@ -21,22 +24,20 @@ class Assignment8Test {
 	public void get_list_of_1000000_numbers_and_number_occurrences() {
 		Assignment8 assignment = new Assignment8();
 		String message = "Starting";
-		System.out.println(message);
-		System.out.println("Thread-"+Thread.currentThread().getName());
+		System.out.println(message);		
 		
 		List<CompletableFuture<Void>> tasks = new ArrayList<>();
 		
 		List<Integer> numbers = Collections.synchronizedList(new ArrayList<>());
 		
 		ExecutorService executor = Executors.newFixedThreadPool(2);
-//		ExecutorService executor = Executors.newCachedThreadPool();
+	//	ExecutorService executor = Executors.newCachedThreadPool();
 		
 		List<Integer> numbersList = assignment.getNumbers();
 		
 		for(int i=0; i< 1000; i++) {
 			CompletableFuture<Void> task = 
-					CompletableFuture.supplyAsync(() -> new Assignment8(), executor)
-					                 .thenApply(a -> numbersList)
+					CompletableFuture.supplyAsync(() -> numbersList, executor)
 					                 .thenAccept(num -> numbers.addAll(num));
 			tasks.add(task);
 		}
@@ -45,18 +46,27 @@ class Assignment8Test {
 				
 		while(tasks.stream()
 		   .filter(CompletableFuture::isDone)
-		   .count() < 1000) 
+		   .count() < 1000)
 			
 			System.out.println(numbers.size() + " numbers added to the list");
 		System.out.println(Arrays.toString(numbers.toArray()));
 		assertEquals(1000000, numbers.size());
-		
-		Map<Integer, Long> numberOccurrences = numbers.stream()
-				   .collect(Collectors.groupingBy(number -> number, Collectors.counting()));
+	    		
+//		Map<Integer, Long> numberOccurrences = numbers.stream()
+//				   .collect(Collectors.groupingBy(number -> number, Collectors.counting()));
 			
-			numberOccurrences.forEach((number, occurrences) -> {
-				System.out.print(number + "=" + occurrences + ", ");
-			}); 
-	  
+//			numberOccurrences.forEach((number, occurrences) -> {
+//				System.out.print(number + "=" + occurrences + ", ");
+//			}); 
+
+	       Map<Integer, Long> count = numbers.stream()
+	                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+	        System.out.println(count);
+	        
+	        //verify the occurrences in another way 
+	        Set<Integer> ns = new HashSet<>(numbers);
+		     for (Integer n : ns)
+		            System.out.println(n + "= " + Collections.frequency(numbers, n));	  
 	}	
 }
